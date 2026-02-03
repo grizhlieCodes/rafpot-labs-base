a9b42b277e9bc29a87f195096c3efd2c9a9aad3e
grizhlieCodes <rafal.potasz@gmail.com>
2026-02-03 15:29:16 +0000

    removed border from section since class works

--- src/lib/components/layout/section/index.ts
+++ src/lib/components/layout/section/index.ts
@@ -1,18 +1,15 @@
 import SectionRoot from './Section.svelte';
 import Content from './SectionContent.svelte';
 import Background from './SectionBackground.svelte';
-import Border from './SectionBorder.svelte';
 
 type SectionComponent = typeof SectionRoot & {
 	Content: typeof Content;
 	Background: typeof Background;
-	Border: typeof Border;
 };
 
 const Section: SectionComponent = Object.assign(SectionRoot, {
 	Content,
 	Background,
-	Border
 });
 
 export default Section;
