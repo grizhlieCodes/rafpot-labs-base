7d8809c885ffac8ce9dd89a415f9f30bc3e4d12f
grizhlieCodes <rafal.potasz@gmail.com>
2026-02-03 15:28:47 +0000

    Created Border components

---
+++ src/lib/components/decorative/BorderX.svelte
@@ -0,0 +1,14 @@
+<script module lang="ts">
+	export const borderStyles: {
+		shared: string;
+		leftBorder: string;
+		rightBorder: string;
+	} = {
+		shared: 'hidden xs:block w-px h-full row-start-1 bg-surface-5 dark:bg-surface-13 transition-colors duration-(--duration-global-cols)',
+		leftBorder: 'col-start-2 justify-self-start',
+		rightBorder: '-col-end-2  justify-self-end'
+	};
+</script>
+
+<span class="{borderStyles.shared} {borderStyles.leftBorder}"></span>
+<span class="{borderStyles.shared} {borderStyles.rightBorder}"></span>
